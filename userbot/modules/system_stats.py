15f60c13dc5bedd1639bf0cf7a16a5b31ac0c802
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-01-26 04:28:44 +0800

    tes nich

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -122,21 +122,20 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern="^.on$")
 async def amireallyalive(on):
     """ For .on command, check if the bot is running.  """
-    await on.edit("`"
-                     "i am ON My Mastor \n\n"
-                     " \n\n"
-                     "botdo is ON \n\n"
-                     f"Telethon version: {version.__version__} \n"
-                     f"Python: {python_version()} \n"
-                     f"------------------------------------ \n"
-                     f"User: {DEFAULTUSER} \n"
-                     " \n\n"
-                     f"Creator: Mayur Karaniya \n"
-                     " \n\n"
-                     f"Owner: 3Cube TeKnoways \n"
-                     " \n\n"
-                     f"Userbot: testuserbot "
-                     "`")    
+    await on.edit(""
+       "~~~~~~~~~~~~~~~~~~~~~~ \n"
+                     "Life is complicated:)"
+                     "~~~~~~~~~~~~~~~~~~~~~~ \n"
+                     "\n\n"
+                     f"Telethon: v{version.__version__} \n"
+                     f"Python: v{python_version()} \n\n"
+              "Fortizer is alive! \n"
+                     f"User: {DEFAULTUSER} \n\n"
+       "Watch your step!😙"
+       "~~~~~~~~~~~~~~~~~~~~~~ \n"
+                     "좋은 하루 되세요"
+                     " \n"
+                 "")
 
 
 
