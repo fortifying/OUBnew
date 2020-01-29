ef4189afabbc8493005848be38ea5ba088df2263
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-01-30 00:02:22 +0800

    deleted permanent Dockerfile

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -3,6 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
+# afk
 """ Userbot module which contains afk-related commands """
 
 from random import choice, randint
