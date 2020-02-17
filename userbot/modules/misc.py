feda6d5c0845decd7e17bab80a2f1cab8fcb6196
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-02-17 19:20:30 +0800

    Updated repo

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -128,7 +128,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "Click [here](https://github.com/fortifying/OUBnew) to open OpenUserBot's GitHub page."
+        "Click [here](https://github.com/fortifying/OUBnew) to open my Mastor git page."
     )
 
 
