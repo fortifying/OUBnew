5c1a791290aeba9c1ab5dbc4a1924e368e68fdeb
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-02-24 20:11:31 +0800

    lol lets try

---
+++ userbot/modules/penis.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+from ..help import add_help_item
+from userbot.events import register
+
+PENIS_TEMPLATE = """
+🍆🍆
+🍆🍆🍆
+  🍆🍆🍆
+    🍆🍆🍆
+     🍆🍆🍆
+       🍆🍆🍆
+        🍆🍆🍆
+         🍆🍆🍆
+          🍆🍆🍆
+          🍆🍆🍆
+      🍆🍆🍆🍆
+ 🍆🍆🍆🍆🍆🍆
+ 🍆🍆🍆  🍆🍆🍆
+    🍆🍆       🍆🍆
+"""
+
+
+@register(outgoing=True, pattern=r"^\.(?:penis|dick)\s?(.)?")
+async def emoji_penis(e):
+    emoji = e.pattern_match.group(1)
+
+    await e.edit("Dickifying...")
+    message = PENIS_TEMPLATE
+    if emoji:
+        message = message.replace('🍆', emoji)
+
+    await e.edit(message)
+
+
+add_help_item(
+    "penis",
+    "Fun",
+    "Creates a penis using the provided emoji, or 🍆 if no emoji is provided.",
+    """
+.penis [emoji]
+    """
+)
