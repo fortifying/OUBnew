a07879d9577566c29117f46dd34027bf02f2c789
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-02-27 18:50:11 +0800

    last commit on deepfry
    Signed-off-by: heinzdf <darkcyborg7@gmail.com>

--- userbot/modules/deepfry.py
+++ userbot/modules/deepfry.py
@@ -4,42 +4,42 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.events import register
 from userbot import bot, CMD_HELP
+from userbot.events import register
 
 @register(outgoing=True, pattern="^.df(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("`Reply to any user message.`")
+       await event.edit("```Reply to any user message.```")
        return
     reply_message = await event.get_reply_message() 
-    if not reply_message.media:
-       await event.edit("```reply to text message```")
+    if not reply_message.text:
+       await event.edit("```Reply to text message```")
        return
     chat = "@image_deepfrybot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("`Reply to actual users message.`")
+       await event.edit("```Reply to actual users message.```")
        return
-    await event.edit("`Processing`")
+    await event.edit("```Making a Quote```")
     async with bot.conversation(chat) as conv:
           try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
               await bot.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("`Please unblock @image_deepfrybot and try again`")
+              await event.reply("```Please unblock @image_deepfrybot and try again```")
               return
-          if response.text.startswith("Forward"):
-             await event.edit("`can you kindly disable your forward privacy settings for good?`")
+          if response.text.startswith("Hi!"):
+             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
           else: 
-             await event.edit(f"{response.message.message}")
-
+             await event.delete()   
+             await bot.forward_messages(event.chat_id, response.message)
 
 CMD_HELP.update({
         "deepfry2": 
         ".df \
-          \nUsage: Krispi image.\n"
+          \nUsage: Krispi image😂👌.\n"
     })
