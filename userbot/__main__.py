eeff2e8cf316ec7620ac299453809f2b1e4f96ce
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-03-14 22:02:35 +0800

    main: update
    Signed-off-by: heinzdf <fortizer@workers.dev>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,12 +26,12 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running OpenUserBot [v7.7.7]")
+LOGS.info("You are running OUBnew-ftzr v3.0")
 
 LOGS.info(
-    "😘 Congratulations, your userbot is now running !! Test it by typing .ping in any chat 😘."
-    "Once died 😵, but now ON again 😉, A BIG thanks to https://github.com/Devp73 Dev Patel who have helped make this userbot ON AGAIN with his HUGE FIX!!"
-    "If you need assistance, head to https://t.me/PPE_Support")
+    "Congrats, your bot have successfully running"
+    "To test your bot type .on or .live"
+    "Don't forget to check update by typing .update! Have fun!")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
