acdb928acfbb12e34590e5dcaa84d9a7f9b94b85
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-03-22 02:40:50 +0800

    modules: added covid-19
    Signed-off-by: HeinzDF <fortizers@gmail.com>

---
+++ userbot/modules/corona_virus.py
@@ -0,0 +1,25 @@
+from covid import Covid
+from userbot import CMD_HELP
+from userbot.events import register
+
+@register(pattern="^.covid(?: |$)(.*)")
+async def _(event):
+    covid = Covid()
+    data = covid.get_data()
+    country = event.pattern_match.group(1)
+    country_data = get_country_data(country, data)
+    output_text = "" 
+    for name, value in country_data.items():
+        output_text += "`{}`: `{}`\n".format(str(name), str(value))
+    await event.edit("**CoronaVirus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
+
+def get_country_data(country, world):
+    for country_data in world:
+        if country_data["country"] == country:
+            return country_data
+    return {"Status": "No information yet about this country!"}
+
+CMD_HELP.update({
+        "covid": ".covid (country)\
+        \nUsage: To get information about COVID-19 in country\n"
+    })
