bc9819f6f004fbbc8b64b2333ae951052d8feaed
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-03-22 03:02:20 +0800

    modules: test aesthetics

---
+++ userbot/modules/aesthetics.py
@@ -0,0 +1,31 @@
+# ported to OUB by fortifying
+from telethon import events
+from userbot import CMD_HELP
+from userbot.events import register
+
+PRINTABLE_ASCII = range(0x21, 0x7f)
+
+
+def aesthetify(string):
+    for c in string:
+        c = ord(c)
+        if c in PRINTABLE_ASCII:
+            c += 0xFF00 - 0x20
+        elif c == ord(" "):
+            c = 0x3000
+        yield chr(c)
+
+#@register(outgoing=True, pattern="^.ae(?: |$)(.*)")
+@register(events.NewMessage(pattern=r'.ae\s+(.+)', outgoing=True))
+@register(events.MessageEdited(pattern=r'.ae\s+(.+)', outgoing=True))
+async def aes(event):
+    text = event.pattern_match.group(1)
+    text = "".join(aesthetify(text))
+    await event.edit(text=text, parse_mode=None, link_preview=False)
+    raise events.StopPropagation
+    
+    CMD_HELP.update({
+        "aes": ".ae for aesthetic\
+        \nUsage: Gatau buat apa anjjg."
+    })
+
