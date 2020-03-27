4e140d2e2ca323cd1fc52b2e24f124ecc66446d4
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-03-28 07:30:50 +0800

    Update __init__.py
    changes from HEROKU_APPNAME to HEROKU_APP_NAME
    
    Signed-off-by: HeinzDF <fortizers@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -77,7 +77,7 @@ PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
 
 # Heroku Credentials for updater.
 HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
-HEROKU_APPNAME = os.environ.get("HEROKU_APPNAME", None)
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
 
 # Custom (forked) repo URL for updater.
