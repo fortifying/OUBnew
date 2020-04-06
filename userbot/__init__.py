97f58dab8c6855d8bb9907e308701873099a07ac
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-04-06 09:19:23 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -90,7 +90,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/fortifying/OUBnew.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "sql-extended")
+    "UPSTREAM_REPO_BRANCH", "phoenix")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
