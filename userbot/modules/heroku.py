531b761332a6070b1b62f157a81b24ac4a047a5f
HeinzDF <60229681+fortifying@users.noreply.github.com>
2020-04-11 08:50:44 +0800

    State changes for several modules
    Aria: known as aurl, added download progress
    google_drive: add support for magnet, Torrent and aurl link
    Heroku: refactors some strings
    
    **you might need to see how to setup GDrive, check out below**
    https://github.com/fortifying/OUBnew/blob/sql-extended/README.md
    
    **Be tester on Phoenix**
    https://github.com/fortifying/OUBnew/tree/phoenix
    
    Signed-off-by: HeinzDF <fortizers@gmail.com>

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -1,18 +1,22 @@
 # Copyright (C) 2020 Adek Maulana.
 # All rights reserved.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
 """
    Heroku manager for your userbot
 """
 
 import heroku3
-import asyncio
 import os
 import requests
 import math
 
 from userbot import CMD_HELP, HEROKU_APP_NAME, HEROKU_API_KEY
 from userbot.events import register
-from userbot.prettyjson import prettyjson
+from userbot.utils.prettyjson import prettyjson
 
 Heroku = heroku3.from_key(HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
@@ -28,20 +32,19 @@ async def variable(var):
         app = Heroku.app(HEROKU_APP_NAME)
     else:
         return await var.edit("`[HEROKU]:"
-                              "\nPlease setup your` **HEROKU_APP_NAME**")
+                              "\nPlease setup your` **HEROKU_APP_NAME**.")
     exe = var.pattern_match.group(1)
     heroku_var = app.config()
     if exe == "get":
         await var.edit("`Getting information...`")
-        await asyncio.sleep(1.5)
         try:
             variable = var.pattern_match.group(2).split()[0]
             if variable in heroku_var:
                 return await var.edit("**ConfigVars**:"
-                                      f"\n\n`{variable} = {heroku_var[variable]}`\n")
+                                      f"\n`{variable}` = `{heroku_var[variable]}`.\n")
             else:
                 return await var.edit("**ConfigVars**:"
-                                      f"\n\n`Error:\n-> {variable} don't exists`")
+                                      f"\n`Error > {variable} don't exists`.")
         except IndexError:
             configs = prettyjson(heroku_var.to_dict(), indent=2)
             with open("configs.json", "w") as fp:
@@ -53,7 +56,7 @@ async def variable(var):
                         var.chat_id,
                         "configs.json",
                         reply_to=var.id,
-                        caption="`Output too large, sending it as a file`",
+                        caption="`Output too large, sending it as a file`.",
                     )
                 else:
                     await var.edit("`[HEROKU]` ConfigVars:\n\n"
@@ -75,24 +78,22 @@ async def variable(var):
                 value = var.pattern_match.group(2).split()[1]
             except IndexError:
                 return await var.edit(">`.set var <ConfigVars-name> <value>`")
-        await asyncio.sleep(1.5)
         if variable in heroku_var:
-            await var.edit(f"**{variable}**  `successfully changed to`  ->  **{value}**")
+            await var.edit(f"**{variable}** `successfully changed to` > **{value}**")
         else:
-            await var.edit(f"**{variable}**  `successfully added with value`  ->  **{value}**")
+            await var.edit(f"**{variable}** `successfully added with value` > **{value}**")
         heroku_var[variable] = value
     elif exe == "del":
         await var.edit("`Getting information to deleting variable...`")
         try:
             variable = var.pattern_match.group(2).split()[0]
         except IndexError:
-            return await var.edit("`Please specify ConfigVars you want to delete`")
-        await asyncio.sleep(1.5)
+            return await var.edit("`Please specify ConfigVars you want to delete`.")
         if variable in heroku_var:
-            await var.edit(f"**{variable}**  `successfully deleted`")
+            await var.edit(f"**{variable}** `successfully deleted`.")
             del heroku_var[variable]
         else:
-            return await var.edit(f"**{variable}**  `is not exists`")
+            return await var.edit(f"**{variable}** `is not exists`.")
 
 
 @register(outgoing=True, pattern=r"^.usage(?: |$)")
@@ -100,7 +101,7 @@ async def dyno_usage(dyno):
     """
         Get your account Dyno Usage
     """
-    await dyno.edit("`Processing...`")
+    await dyno.edit("`Getting Information...`")
     useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
                  'AppleWebKit/537.36 (KHTML, like Gecko) '
                  'Chrome/80.0.3987.149 Mobile Safari/537.36'
@@ -140,13 +141,11 @@ async def dyno_usage(dyno):
     AppHours = math.floor(AppQuotaUsed / 60)
     AppMinutes = math.floor(AppQuotaUsed % 60)
 
-    await asyncio.sleep(1.5)
-
     return await dyno.edit("**Dyno Usage**:\n\n"
                            f" -> `Dyno usage for`  **{HEROKU_APP_NAME}**:\n"
                            f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
-                           f"**|**  [`{AppPercentage}`**%**]"
-                           "\n\n"
+                           f"**|**  [`{AppPercentage}`**%**]\n"
+                           "------------------------------------------------------\n"
                            " -> `Dyno hours quota remaining this month`:\n"
                            f"     •  `{hours}`**h**  `{minutes}`**m**  "
                            f"**|**  [`{percentage}`**%**]"
@@ -155,15 +154,15 @@ async def dyno_usage(dyno):
 
 CMD_HELP.update({
     "heroku":
-    ">.`usage`"
+    ".usage"
     "\nUsage: Check your heroku dyno hours remaining"
-    "\n\n>`.set var <NEW VAR> <VALUE>`"
+    "\n\n.set var <NEW VAR> <VALUE>"
     "\nUsage: add new variable or update existing value variable"
     "\n!!! WARNING !!!, after setting a variable the bot will restarted"
-    "\n\n>`.get var or .get var <VAR>`"
+    "\n\n.get var or .get var <VAR>"
     "\nUsage: get your existing varibles, use it only on your private group!"
     "\nThis returns all of your private information, please be caution..."
-    "\n\n>`.del var <VAR>`"
+    "\n\n.del var <VAR>"
     "\nUsage: delete existing variable"
     "\n!!! WARNING !!!, after deleting variable the bot will restarted"
 })
