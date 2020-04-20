8d9a1cbbeb7ad0d43b2571c951a1ad48bac8b865
Adek Maulana <adek@techdro.id>
2020-04-20 15:17:02 +0700

    modules: .df: add level for deepfryer image
    Change-Id: I33eef26095f405ecf20bdac062cea1b697c95806

--- userbot/modules/deepfryer.py
+++ userbot/modules/deepfryer.py
@@ -1,3 +1,4 @@
+# Ported to OUB by fortifying
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
@@ -5,6 +6,9 @@
 #
 
 # Original source for the deepfrying code (used under the following license): https://github.com/Ovyerus/deeppyer
+import os
+import asyncio
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 # MIT License
 #
@@ -55,6 +59,40 @@ async def deepfryer(event):
 
         if isinstance(data, bool):
             await event.edit("`I can't deep fry that!`")
+@register(outgoing=True, pattern=r'^.df(:? |$)(\d)?')
+async def _(fry):
+    await fry.edit("`Sending information...`")
+    level = fry.pattern_match.group(2)
+    if fry.fwd_from:
+        return
+    if not fry.reply_to_msg_id:
+        await fry.edit("`Reply to any user message photo...`")
+        return
+    reply_message = await fry.get_reply_message()
+    if not reply_message.media:
+        await fry.edit("`No image found to fry...`")
+        return
+    if reply_message.sender.bot:
+        await fry.edit("`Reply to actual user...`")
+        return
+    chat = "@image_deepfrybot"
+    message_id_to_reply = fry.message.reply_to_msg_id
+    async with fry.client.conversation(chat) as conv:
+        try:
+            msg = await conv.send_message(reply_message)
+            if level:
+                m = f"/deepfry {level}"
+                msg_level = await conv.send_message(
+                          m,
+                          reply_to=msg.id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            else:
+                response = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await fry.reply("`Please unblock` @image_deepfrybot`...`")
             return
     else:
         await event.edit("`Reply to an image or sticker to deep fry it!`")
@@ -122,8 +160,28 @@ async def check_media(reply_message):
             data = reply_message.media.document
         else:
             return False
-    else:
-        return False
+            downloaded_file_name = await fry.client.download_media(
+                                 response.media,
+                                 TEMP_DOWNLOAD_DIRECTORY
+            )
+            await fry.client.send_file(
+                fry.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                reply_to=message_id_to_reply
+            )
+            """ - cleanup chat after completed - """
+            try:
+                msg_level
+            except NameError:
+                await fry.client.delete_messages(conv.chat_id,
+                                                 [msg.id, response.id])
+            else:
+                await fry.client.delete_messages(
+                    conv.chat_id,
+                    [msg.id, response.id, r.id, msg_level.id])
+    await fry.delete()
+    return os.remove(downloaded_file_name)
 
     if not data or data is None:
         return False
@@ -134,4 +192,7 @@ CMD_HELP.update({
     "deepfry":
     ".deepfry [number]\
     \n Usage: Reply to an image or sticker to deepfry with value, more value more krispy."
+    ">`.df` or >`.df [level(1-8)]`"
+    "\nUsage: deepfry image/sticker from the reply."
+    "\n@image_deepfrybot"
 })
