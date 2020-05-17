5297c1fe8d6123bf1506685af17439db7c479af2
Heinz Der Flugel <heinzdf@outlook.com>
2020-05-17 12:16:39 +0800

    __init__: remove duplicate environ
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -103,8 +103,6 @@ G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
 G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
 G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
 G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID", None)
-TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY",
-                                         "./downloads/")
 
 # SQL Database URI
 DB_URI = os.environ.get("DATABASE_URL", None)
