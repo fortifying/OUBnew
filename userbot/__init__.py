d39715710f83459271900def8e965f460a6364a8
Heinz Der Flugel <heinzdf@outlook.com>
2020-05-17 12:13:41 +0800

    lah udah di komit kok masih sama
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -169,8 +169,8 @@ G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
 G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
 G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
 GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY",
-                                         "./downloads")
+TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY",
+                                         "./downloads/")
 										 
 # Terminal alias
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "OUBnew")
