29239c46c371b3d947baafbb6ba5e53e03c5df65
Heinz Der Flugel <heinzdf@outlook.com>
2020-05-21 16:39:54 +0800

    getmusic: merge SpotifyDownloader and delete old smd modules
    too much modules, let's cleanup and merged it into one modules
    
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -3,13 +3,17 @@
 from telethon import events
 import subprocess, instantmusic
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+ 
 import io
 import asyncio
 import time
 from userbot.events import register
-from userbot import CMD_HELP
+from userbot import bot, CMD_HELP
 import glob
 import os
+import datetime
  
 os.system("rm -rf *.mp3")
  
@@ -44,9 +48,35 @@ async def _(event):
     subprocess.check_output("rm -rf *.mp3",shell=True)
     await event.delete()
  
+ 
+@register(outgoing=True, pattern="^.smd(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@SpotifyMusicDownloaderBot"
+    await event.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              await bot.send_message(chat, link)
+              respond = await response
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
+              return
+          await event.delete()
+          await bot.forward_messages(event.chat_id, respond.message)
+          await bot.send_read_acknowledge(event.chat_id)
+ 
+ 
 CMD_HELP.update({
     "song":
         ">`.song` **atrist title**"
-        "\nUsage: Finding and uploading song."
+        "\nUsage: Finding and uploading song.\n"
+        ">`.smd` **<song tittle>**"
+        "\nUsage: **Download music from spotify**"
 })
  
