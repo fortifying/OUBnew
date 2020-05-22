e34de1acfaf598e5a60ebad334667d0237c222bb
Heinz Der Flugel <heinzdf@outlook.com>
2020-05-22 19:02:09 +0800

    adzan: fallback: generate infos
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/modules/adzan.py
+++ userbot/modules/adzan.py
@@ -4,54 +4,54 @@ import requests
 from userbot import CMD_HELP
 from userbot.events import register
 
-PLACE = ''
+TEMPAT = ''
 
-
-@register(outgoing=True, pattern=r"^.adzan (.*)")
+@register(pattern="^.adzan(?: |$)(.*)")
 async def get_adzan(adzan):
-    await adzan.edit("🚛Loading...")
+    if not adzan.text.startswith("."):
+        return ""
+
     if not adzan.pattern_match.group(1):
-        LOCATION = PLACE
-        if not LOCATION:
+        LOKASI = TEMPAT
+        if not LOKASI:
             await adzan.edit("Please specify a city or a state.")
             return
     else:
-        LOCATION = adzan.pattern_match.group(1)
+        LOKASI = adzan.pattern_match.group(1)
 
-    url = f'http://muslimsalat.com/{LOCATION}.json?key=2e78acba015745177969e7a03ea46c1a'
-    # url = f'https://api.pray.zone/v2/times/today.json?city={LOCATION}'
+    url = f'http://muslimsalat.com/{LOKASI}.json?key=2e78acba015745177969e7a03ea46c1a'
     request = requests.get(url)
-    if request.status_code == 500:
-        return await adzan.edit(f"Couldn't find city `{LOCATION}`")
+    result = json.loads(request.text)
 
-    parsed = json.loads(request.text)
+    if request.status_code != 200:
+        await adzan.edit(f"{result['status_description']}")
+        return
 
-    city = parsed["results"]["location"]["city"]
-    country = parsed["results"]["location"]["country"]
-    timezone = parsed["results"]["location"]["timezone"]
-    date = parsed["results"]["datetime"][0]["date"]["gregorian"]
+    tanggal = result["items"][0]["date_for"]
+    lokasi = result["query"]
+    lokasi2 = result["country"]
+    lokasi3 = result["address"]
+    lokasi4 = result["state"]
 
-    imsak = parsed["results"]["datetime"][0]["times"]["Imsak"]
-    subuh = parsed["results"]["datetime"][0]["times"]["Fajr"]
-    zuhur = parsed["results"]["datetime"][0]["times"]["Dhuhr"]
-    ashar = parsed["results"]["datetime"][0]["times"]["Asr"]
-    maghrib = parsed["results"]["datetime"][0]["times"]["Maghrib"]
-    isya = parsed["results"]["datetime"][0]["times"]["Isha"]
+    subuh = result["items"][0]["fajr"]
+    syuruk = result["items"][0]["shurooq"]
+    zuhur = result["items"][0]["dhuhr"]
+    ashar = result["items"][0]["asr"]
+    maghrib = result["items"][0]["maghrib"]
+    isya = result["items"][0]["isha"]
 
-    result = (f"**Jadwal Sholat**:\n"
-                 f"📅 `{date}`\n"
-                 f"🌏 `{city} | {country}`\n\n"
-                 f"**Imsak :** `{imsak}`\n"
-                 f"**Subuh :** `{subuh}`\n"
-                 f"**Zuhur :** `{zuhur}`\n"
-                 f"**Ashar :** `{ashar}`\n"
-                 f"**Maghrib :** `{maghrib}`\n"
-                 f"**Isya :** `{isya}`\n")
+    textkirim = (f"⏱  **Jadwal Sholat Pada ** `{tanggal}` : \n" +
+                 f"`{lokasi} | {lokasi2} | {lokasi3} | {lokasi4}`\n\n" +
+                 f"**Subuh : ** `{subuh}`\n" +
+                 f"**Syuruk : ** `{syuruk}`\n" +
+                 f"**Zuhur : ** `{zuhur}`\n" +
+                 f"**Ashar : ** `{ashar}`\n" +
+                 f"**Maghrib : ** `{maghrib}`\n" +
+                 f"**Isya : ** `{isya}`\n")
 
-    await adzan.edit(result)
+    await adzan.edit(textkirim)
 
 CMD_HELP.update({
-        "adzan": ">`.adzan` **city**\
-        \nUsage: Gets the prayer time for moslem."
+        "adzan": "`>.adzan <city> or .adzan <country>`\
+        \nUsage: Gets the prayer time for muslim.\n"
     })
-
