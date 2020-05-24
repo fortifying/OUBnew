2fdc9d69bd96868325f43882c4f5f4479e08a59a
yincen17 <52388255+yincen17@users.noreply.github.com>
2020-05-23 21:40:09 -0700

    trying fix instagram.py

--- userbot/modules/instagram.py
+++ userbot/modules/instagram.py
@@ -4,7 +4,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 
-from instalooter.looters import ProfileLooter
+from instalooter.looters import ProfileLooter , InstaLooter
 import os
 import time
 import asyncio
