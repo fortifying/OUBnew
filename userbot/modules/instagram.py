45351ea27e1c9d00cc757b79dff8d20461d7d6c0
yincen17 <52388255+yincen17@users.noreply.github.com>
2020-05-23 21:59:58 -0700

    trying fix instagram.py

--- userbot/modules/instagram.py
+++ userbot/modules/instagram.py
@@ -77,7 +77,7 @@ def time_formatter(milliseconds: int) -> str:
 @register(outgoing=True, pattern=r"^.ig(?: |$)(.*)")
 async def instagram_dl(igdl):
     """ To downloading photos from instagram account """
-    uname_ig = igdl.pattern_match.group(1)
+    uname = igdl.pattern_match.group(1)
     input_str = TEMP_DOWNLOAD_DIRECTORY
     if not os.path.exists(input_str):
         os.makedirs(input_str)
