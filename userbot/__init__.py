c1856732d079b77a27bebdab020b44cadcb594de
Heinz Der Flugel <heinzdf@outlook.com>
2020-05-30 17:12:52 +0800

    __init__: import G_BAN_LOGGER_GROUP
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -102,6 +102,11 @@ G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
 G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
 G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID", None)
 
+# Send .chatid in any group with all your administration bots (added)
+G_BAN_LOGGER_GROUP = os.environ.get("G_BAN_LOGGER_GROUP", None)
+if G_BAN_LOGGER_GROUP:
+        G_BAN_LOGGER_GROUP = int(G_BAN_LOGGER_GROUP)
+
 # SQL Database URI
 DB_URI = os.environ.get("DATABASE_URL", None)
 
