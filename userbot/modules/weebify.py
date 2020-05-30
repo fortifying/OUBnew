04ff5d1edaca9b3bc7a4959e71ac277454523aae
Heinz Der Flugel <heinzdf@outlook.com>
2020-05-30 17:54:45 +0800

    Proper fix and add some required string
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/modules/weebify.py
+++ userbot/modules/weebify.py
@@ -1,5 +1,5 @@
 #Inspired by Saitama Bot, credits - @kirito6969, @PhycoNinja13b
-#Modified by @deleteduser420 for OUB-X
+#Thx to @DeletedUser420
 
 
 from userbot.events import register
@@ -159,15 +159,15 @@ async def xcursive(cursivelite):
     
 
     
-#     CMD_HELP.update({
-#     "textx":
-#     "Usage: .font <text>\
-#       \n`.weeb` Weebify a text\
-#     \n\n`.cursive` make text cursive.\
-#     \n\n`.cursivebold` make text cursive bold.\
-#     \n\n`.medieval` make text medival.\
-#     \n\n`.medievalbold` make text medival bold.\
-#     \n\n`.doublestruck` make text doublestruck.\
-#     \n\n`.bold` make text bold."
+     CMD_HELP.update({
+     "textx":
+     "Usage: .font <text>\
+       \n`.weeb` Weebify a text\
+     \n\n`.cursive` make text cursive.\
+     \n\n`.cursivebold` make text cursive bold.\
+     \n\n`.medieval` make text medival.\
+     \n\n`.medievalbold` make text medival bold.\
+     \n\n`.doublestruck` make text doublestruck.\
+     \n\n`.bold` make text bold."
    
-# })
+ })
