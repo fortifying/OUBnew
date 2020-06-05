4c3f13ceb274fe94f597cb6454e4d5088e4eabc5
Heinz Der Flugel <heinzdf@outlook.com>
2020-06-05 21:30:10 +0800

    AFK: change state
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -163,8 +163,9 @@ async def mention_afk(mention):
                 afk_since = f"`{int(seconds)}s` ago"
             if mention.sender_id not in USERS:
                 if AFKREASON:
-                    await mention.reply(f"**My mastor is not here for a while since** {afk_since}.\
-                        \nReason: `{AFKREASON}`")
+                    await mention.reply(str(choice(AFKSTR)
+                           f"\n\nI'm AFK right now since {afk_since}"
+                           f"\nReason: `{AFKREASON}`"))
                 else:
                     await mention.reply(str(choice(AFKSTR)))
                 USERS.update({mention.sender_id: 1})
@@ -240,8 +241,9 @@ async def afk_on_pm(sender):
                 afk_since = f"`{int(seconds)}s` ago"
             if sender.sender_id not in USERS:
                 if AFKREASON:
-                    await sender.reply(f"**My mastor is not here for a while since** {afk_since}.\
-                        \nReason: `{AFKREASON}`")
+                    await sender.reply(str(choice(AFKSTR)
+                           f"\n\nI'm AFK right now since {afk_since}"
+                           f"\nReason: `{AFKREASON}`"))
                 else:
                     await sender.reply(str(choice(AFKSTR)))
                 USERS.update({sender.sender_id: 1})
