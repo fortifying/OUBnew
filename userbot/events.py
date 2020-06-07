79d74c1fefb411fe0d3ca0f8a93801ba8226873e
Heinz Der Flugel <heinzdf@outlook.com>
2020-06-08 07:47:47 +0800

    events: send error report as txt Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/events.py
+++ userbot/events.py
@@ -7,17 +7,16 @@
  One of the main components of the userbot. """
 
 import sys
+ 
 from asyncio import create_subprocess_shell as asyncsubshell
 from asyncio import subprocess as asyncsub
 from os import remove
 from time import gmtime, strftime
 from traceback import format_exc
-
 from telethon import events
-
-from userbot import bot, BOTLOG_CHATID, LOGSPAMMER
-
-
+ 
+from userbot import bot, BOTLOG, BOTLOG_CHATID, LOGSPAMMER, BLACKLIST
+ 
 def register(**args):
     """ Register a new event. """
     pattern = args.get('pattern', None)
@@ -26,89 +25,79 @@ def register(**args):
     unsafe_pattern = r'^[^/!#@\$A-Za-z]'
     groups_only = args.get('groups_only', False)
     trigger_on_fwd = args.get('trigger_on_fwd', False)
+    trigger_on_inline = args.get('trigger_on_inline', False)
     disable_errors = args.get('disable_errors', False)
     insecure = args.get('insecure', False)
-
+    me = bot.get_me()
+    uid = me.id
+    uid not in BLACKLIST
+ 
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
-
+ 
     if "disable_edited" in args:
         del args['disable_edited']
-
+ 
     if "ignore_unsafe" in args:
         del args['ignore_unsafe']
-
+ 
     if "groups_only" in args:
         del args['groups_only']
-
+ 
     if "disable_errors" in args:
         del args['disable_errors']
-
+ 
     if "trigger_on_fwd" in args:
         del args['trigger_on_fwd']
-
+ 
+    if "trigger_on_inline" in args:
+        del args['trigger_on_inline']
+ 
     if "insecure" in args:
         del args['insecure']
-
+ 
     if pattern:
         if not ignore_unsafe:
             args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
-
+ 
     def decorator(func):
         async def wrapper(check):
             if check.edit_date and check.is_channel and not check.is_group:
-                # Messages sent in channels can be edited by other users.
-                # Ignore edits that take place in channels.
                 return
-            if not LOGSPAMMER:
-                send_to = check.chat_id
-            else:
-                send_to = BOTLOG_CHATID
-
-            if not trigger_on_fwd and check.fwd_from:
-                return
-
+ 
             if groups_only and not check.is_group:
-                await check.respond("`I don't think this is a group.`")
+                await check.respond("`Are you sure this is a group?`")
                 return
-
             if check.via_bot_id and not insecure and check.out:
                 return
-
+ 
             try:
-                await func(check)
-
-            # Thanks to @kandnub for this HACK.
-            # Raise StopPropagation to Raise StopPropagation
-            # This needed for AFK to working properly
-
+                if uid not in BLACKLIST:
+                    await func(check)
+                else:
+                    raise RetardsException()
             except events.StopPropagation:
                 raise events.StopPropagation
-            # This is a gay exception and must be passed out. So that it doesnt spam chats
+            except RetardsException:
+                exit(1)
             except KeyboardInterrupt:
                 pass
-            except BaseException:
-
-                # Check if we have to disable it.
-                # If not silence the log spam on the console,
-                # with a dumb except.
-
+            except:
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
+ 
                     text = "**USERBOT ERROR REPORT**\n"
                     link = "Support chat PM: @fortifying"
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
                     text += "Nothing is logged except the fact of error and date\n"
-
+ 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
                     ftext += "\nwe logged only fact of error and date,"
                     ftext += "\nwe respect your privacy,"
                     ftext += "\nyou may not report this error if you've"
                     ftext += "\nany confidential data here, no one will see your data\n"
-                    ftext += "\nSUPPORT CHAT PM: @heinzdf"
                     ftext += "================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nDate: " + date
@@ -121,40 +110,38 @@ def register(**args):
                     ftext += "\n\nError text:\n"
                     ftext += str(sys.exc_info()[1])
                     ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
-
+ 
                     command = "git log --pretty=format:\"%an: %s\" -10"
-
+ 
                     ftext += "\n\n\nLast 10 commits:\n"
-
+ 
                     process = await asyncsubshell(command,
                                                   stdout=asyncsub.PIPE,
                                                   stderr=asyncsub.PIPE)
                     stdout, stderr = await process.communicate()
                     result = str(stdout.decode().strip()) \
                         + str(stderr.decode().strip())
-
+ 
                     ftext += result
-
-                    file = open("error.log", "w+")
+ 
+                    file = open("crash.txt", "w+")
                     file.write(ftext)
                     file.close()
-
-                    if LOGSPAMMER:
-                        await check.client.respond(
-                            "`Sorry, my userbot has crashed."
-                            "\nThe error logs are stored in the userbot's log chat.`"
-                        )
-
-                        await check.client.send_file(send_to,
-                                                     "crash.txt",
-                                                     caption=text)
-                        remove("crash.txt")
+ 
+                    await check.client.send_file(BOTLOG_CHATID
+                                                if BOTLOG
+                                                else check.chat_id, "crash.txt", caption=text)
+                    remove("crash.txt")
+ 
             else:
                 pass
-
+ 
         if not disable_edited:
             bot.add_event_handler(wrapper, events.MessageEdited(**args))
         bot.add_event_handler(wrapper, events.NewMessage(**args))
         return wrapper
-
+ 
     return decorator
+ 
+class RetardsException(Exception):
+    pass
