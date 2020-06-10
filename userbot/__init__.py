5f79e9d941e4dd34261ce7846e48e88bca00dbc2
azrim <mirzaspc@gmail.com>
2020-06-10 15:10:39 +0700

    modules: zipfile: add support to compress multiple files
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -4,25 +4,25 @@
 # you may not use this file except in compliance with the License.
 #
 """ Userbot initialization. """
-
+ 
 import os
-
+ 
 from sys import version_info
 from logging import basicConfig, getLogger, INFO, DEBUG
 from distutils.util import strtobool as sb
-
+ 
 from pylast import LastFMNetwork, md5
 from pySmartDL import SmartDL
 from dotenv import load_dotenv
 from requests import get
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-
+ 
 load_dotenv("config.env")
-
+ 
 # Bot Logs setup:
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
-
+ 
 if CONSOLE_LOGGER_VERBOSE:
     basicConfig(
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -32,125 +32,129 @@ else:
     basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
                 level=INFO)
 LOGS = getLogger(__name__)
-
+ 
 if version_info[0] < 3 or version_info[1] < 8:
     LOGS.info("You MUST have a python version of at least 3.8."
               "Multiple features depend on this. Bot quitting.")
     quit(1)
-
+ 
 # Check if the config was edited by using the already used variable.
 # Basically, its the 'virginity check' for the config file ;)
 CONFIG_CHECK = os.environ.get(
     "", None)
-
+ 
 if CONFIG_CHECK:
     LOGS.info(
         "Please remove the line mentioned in the first hashtag from the config.env file"
     )
     quit(1)
-
+ 
 # Telegram App KEY and HASH
 API_KEY = os.environ.get("API_KEY", None)
 API_HASH = os.environ.get("API_HASH", None)
-
+ 
 # Userbot Session String
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
-
+ 
 # Logging channel/group ID configuration.
 BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", None))
-
+ 
 # Github Credentials for updater and Gitupload.
 GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
 GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
-
+ 
 # Userbot logging feature switch.
 BOTLOG = sb(os.environ.get("BOTLOG", "False"))
 LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "False"))
-
+ 
 # Genius lyrics get this value from https://genius.com/developers both has same values
 GENIUS_API_TOKEN = os.environ.get("GENIUS", None)
 # Genius lyrics get this value from https://genius.com/developers both has same values
 GENIUS = os.environ.get("GENIUS_API_TOKEN", None)
-
+ 
 # Quotes API Token
 QUOTES_API_TOKEN = os.environ.get("QUOTES_API_TOKEN", None)
-
+ 
 # Bleep Blop, this is a bot ;)
 PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
-
+ 
 # Heroku Credentials for updater.
 HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-
+ 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/fortifying/OUBnew.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "sql-extended")
-
+ 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
-
+ 
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
-
+ 
 # Google Drive Module
 G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
 G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
 G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
 G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID", None)
-
+ 
+# Zipfile module
+ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY",
+                                         "./zips")
+ 
 # Send .chatid in any group with all your administration bots (added)
 G_BAN_LOGGER_GROUP = os.environ.get("G_BAN_LOGGER_GROUP", None)
 if G_BAN_LOGGER_GROUP:
         G_BAN_LOGGER_GROUP = int(G_BAN_LOGGER_GROUP)
-
+ 
 # SQL Database URI
 DB_URI = os.environ.get("DATABASE_URL", None)
-
+ 
 # OCR API key
 OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
-
+ 
 # remove.bg API key
 REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-
+ 
 # Chrome Driver and Headless Google Chrome Binaries
 CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
 GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
-
+ 
 # OpenWeatherMap API Key
 OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
 WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY", None)
-
+ 
 # Lydia API
 LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
-
+ 
 # Anti Spambot Config
 ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
 ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
-
+ 
 # Youtube API key
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-
+ 
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
-
+ 
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", ""))
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
-
+ 
 # Clean Welcome
 CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
-
+ 
 # Screenshot web access key
 SS_LAYER_KEY = os.environ.get("SS_LAYER_KEY", None)
-
+ 
 # Last.fm Module
 BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
 DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-
+ 
 LASTFM_API = os.environ.get("LASTFM_API", None)
 LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
 LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
@@ -163,11 +167,11 @@ if LASTFM_API and LASTFM_SECRET and LASTFM_USERNAME and LASTFM_PASS:
                            password_hash=LASTFM_PASS)
 else:
     lastfm = None
-
-
+ 
+ 
 # bit.ly module
 BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
-
+ 
 # Google Drive Module
 G_DRIVE_DATA = os.environ.get("G_DRIVE_DATA", None)
 G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
@@ -179,27 +183,27 @@ TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY",
 										 
 # Terminal alias
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "OUBnew")
-
+ 
 # JustWatch Country
 WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", None)
-
+ 
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
 # and giving them correct perms to work properly.
 if not os.path.exists('bin'):
     os.mkdir('bin')
-
+ 
 binaries = {
     "https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown":
     "bin/megadown",
     "https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py":
     "bin/cmrudl"
 }
-
+ 
 for binary, path in binaries.items():
     downloader = SmartDL(binary, path, progress_bar=False)
     downloader.start()
     os.chmod(path, 0o755)
-
+ 
 # 'bot' variable
 if STRING_SESSION:
     # pylint: disable=invalid-name
@@ -207,32 +211,32 @@ if STRING_SESSION:
 else:
     # pylint: disable=invalid-name
     bot = TelegramClient("userbot", API_KEY, API_HASH)
-
-
+ 
+ 
 async def check_botlog_chatid():
     if not BOTLOG_CHATID and LOGSPAMMER:
         LOGS.info(
             "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the private error log storage to work."
         )
         quit(1)
-
+ 
     elif not BOTLOG_CHATID and BOTLOG:
         LOGS.info(
             "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the userbot logging feature to work."
         )
         quit(1)
-
+ 
     elif not BOTLOG or not LOGSPAMMER:
         return
-
+ 
     entity = await bot.get_entity(BOTLOG_CHATID)
     if entity.default_banned_rights.send_messages:
         LOGS.info(
             "Your account doesn't have rights to send messages to BOTLOG_CHATID "
             "group. Check if you typed the Chat ID correctly.")
         quit(1)
-
-
+ 
+ 
 with bot:
     try:
         bot.loop.run_until_complete(check_botlog_chatid())
@@ -241,11 +245,11 @@ with bot:
             "BOTLOG_CHATID environment variable isn't a "
             "valid entity. Check your environment variables/config.env file.")
         quit(1)
-
+ 
 # Userbot ver/tag/codename
 OUBnew_VER = "4.0"
 CODENAME = "Meltdown"
-
+ 
 # Global Variables
 COUNT_MSG = 0
 USERS = {}
@@ -255,3 +259,4 @@ CMD_HELP = {}
 ZALG_LIST = {}
 ISAFK = False
 AFKREASON = None
+ 
--- userbot/modules/zipfile.py
+++ userbot/modules/zipfile.py
@@ -6,9 +6,10 @@ import asyncio
 import zipfile
 from pySmartDL import SmartDL
 from userbot.events import register
+from datetime import date
 import time
 import os
-from userbot import TEMP_DOWNLOAD_DIRECTORY ,bot
+from userbot import TEMP_DOWNLOAD_DIRECTORY, ZIP_DOWNLOAD_DIRECTORY, bot
 from userbot import CMD_HELP
 # from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
 from userbot.util import admin_cmd, humanbytes, progress, time_formatter
@@ -18,7 +19,7 @@ from userbot.util import admin_cmd, humanbytes, progress, time_formatter
 async def _(event):
     #Prevent Channel Bug to use update
     if event.is_channel and not event.is_group:
-        await event.edit("`compress Commad isn't permitted on channels`")
+        await event.edit("`compress Command isn't permitted on channels`")
         return
     if event.fwd_from:
         return
@@ -57,6 +58,65 @@ async def _(event):
     await event.delete()
 
 
+@register(outgoing=True, pattern=r"^.addzip(?: |$)(.*)")
+async def addzip(add):
+    """ Add file to zip list """
+    #Prevent Channel Bug to use update
+    if add.is_channel and not add.is_group:
+        await event.edit("`compress Command isn't permitted on channels`")
+        return
+    if add.fwd_from:
+        return
+    if not add.is_reply:
+        await add.edit("Reply to a file to compress it.")
+        return
+    directroy_zip = zip
+    mone = await add.edit("Processing ...")
+    if not os.path.isdir(ZIP_DOWNLOAD_DIRECTORY):
+        os.makedirs(ZIP_DOWNLOAD_DIRECTORY)
+    if add.reply_to_msg_id:
+        reply_message = await add.get_reply_message()
+        try:
+            c_time = time.time()
+            downloaded_file_name = await bot.download_media(
+                reply_message,
+                ZIP_DOWNLOAD_DIRECTORY,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, mone, c_time, "trying to download")
+                )
+            )
+            directory_name = downloaded_file_name
+            await add.edit(downloaded_file_name)
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await mone.edit(str(e))
+            return
+
+
+@register(outgoing=True, pattern=r"^.upzip(?: |$)(.*)")
+async def upload_zip(up):
+    if not os.path.isdir(ZIP_DOWNLOAD_DIRECTORY):
+        await up.edit(f"`Files not found`")
+        return
+    input_str = up.pattern_match.group(1)
+    curdate = today.strftime("%m%d%y")
+    if input_str:
+        title = str(input_str)
+    else:
+        title = "zipfile" + f"{curdate}"
+    zipf = zipfile.ZipFile(title + '.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir(ZIP_DOWNLOAD_DIRECTORY, zipf)
+    zipf.close()
+    await bot.send_file(
+        up.chat_id,
+        title + ".zip",
+        caption="Zipped By EyePatch",
+        force_document=True,
+        allow_cache=False,
+        reply_to=event.message.id,
+    )
+    os.rmdir(ZIP_DOWNLOAD_DIRECTORY)
+
+
 def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
@@ -66,7 +126,11 @@ def zipdir(path, ziph):
 
 
 CMD_HELP.update({
-        "compress":
-        ".compress [optional: <reply to file >]\
+        "zipfile":
+        ">`.compress` [optional: <reply to file >]\
             \nUsage: make files to zip."
+        "\n>`.addzip` <reply to file >\
+            \nUsage: add files to zip list."
+        "\n>`.upzip` [optional: <zip title>]\
+            \nUsage: upload zip list."
 })
