bc3fe44d78ebd93198a04cce1e1c14e1aec4f366
Heinz Der Flugel <heinzdf@outlook.com>
2020-06-18 03:20:27 +0800

    stickers: fix derp Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -19,7 +19,7 @@ from telethon.tl.types import InputStickerSetID
 from telethon.tl.types import DocumentAttributeSticker
 
 KANGING_STR = [
-    "`Packing this sticker..`",
+    "Packing this sticker..",
 ]
 
 
