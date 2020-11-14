6a7c3af40342ad2ee9fd9cbcd0e29261abde2f09
rizgustiadi <50516841+rizgustiadi@users.noreply.github.com>
2020-11-14 09:42:08 +0700

    Update quote.py
    Quote

--- userbot/modules/quote.py
+++ userbot/modules/quote.py
@@ -23,7 +23,7 @@ async def _(event):
     if reply_message.sender.bot:
        await event.edit("```Reply to actual users message.```")
        return
-    await event.edit("```Making a Quote```")
+    await event.edit("```Sticker Sedang dibuat Sabarr satttt....```")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
